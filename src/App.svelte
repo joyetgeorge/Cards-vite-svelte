7b02e28579680d89faa0ac2c1dc6630d4b59dd22
joyetgeorge <joyetgeorge07@gmail.com>
2023-01-22 00:43:07 +0530

    Added card component with styling
    Signed-off-by: joyetgeorge <joyetgeorge07@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,44 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Card from './lib/Card.svelte'
+
+  let data = [
+    {
+      icon: 'https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/48/0471FF/external-clipboard-accounting-tanah-basah-basic-outline-tanah-basah.png',
+      label: 'Project management',
+      description: 'Create tasks, track time, and update progress all in one place'
+    },
+    {
+      icon: 'https://img.icons8.com/fluency-systems-regular/48/CF00FF/clock--v3.png',
+      label: 'Time tracking',
+      description: 'Discover how much time your team is spending on their work.'
+    },
+    {
+      icon: 'https://img.icons8.com/material-rounded/48/FF7600/tear-off-calendar.png',
+      label: 'Resource planning',
+      description: 'Monitor how your resources are utilized across projects.'
+    },
+    {
+      icon: 'https://img.icons8.com/fluency-systems-regular/48/02B94C/us-dollar-circled--v1.png',
+      label: 'invoicing',
+      description: 'Save time by invoicing based on reported time for accurate billing.'
+    }
+  ]
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="cards">
+    {#each data as item, index}
+       <Card item={item} index={index}/>
+    {/each}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .cards {
+    height: 100vh;
+    display: flex;
+    justify-content: space-evenly;
+    gap: 2rem;
+    align-items: center;
   }
 </style>
